2809b88babf96fe6b03684f0f323659f8ec87895
pape sembene <sembenpape4@gmail.com>
2025-07-22 19:43:06 +0000

    test deploiement

--- index.php
+++ index.php
@@ -1,28 +1,29 @@
 <?php
-use App\Core\Router;
+// use App\Core\Router;
 
-require_once __DIR__ . '/vendor/autoload.php';
+// require_once __DIR__ . '/vendor/autoload.php';
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
+// header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+// // header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
+// if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+//     http_response_code(200);
+//     exit();
+// }
 
-$routes = require __DIR__ . '/routes/route.api.php';
+// $routes = require __DIR__ . '/routes/route.api.php';
 
-try {
-    Router::resolve($routes);
-} catch (\Exception $e) {
-    http_response_code($e->getCode() ?: 500);
-    echo json_encode([
-        'data' => null,
-        'statut' => 'error',
-        'code' => $e->getCode() ?: 500,
-        'message' => $e->getMessage(),
-        'timestamp' => date('Y-m-d H:i:s')
-    ], JSON_UNESCAPED_UNICODE);
-}
+// try {
+//     Router::resolve($routes);
+// } catch (\Exception $e) {
+//     http_response_code($e->getCode() ?: 500);
+//     echo json_encode([
+//         'data' => null,
+//         'statut' => 'error',
+//         'code' => $e->getCode() ?: 500,
+//         'message' => $e->getMessage(),
+//         'timestamp' => date('Y-m-d H:i:s')
+//     ], JSON_UNESCAPED_UNICODE);
+// }
+echo 'bienvenue dans l\'API AppDAF';
