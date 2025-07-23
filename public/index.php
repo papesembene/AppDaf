17c476186912c2c2b836edcd8d6b16989a6ef3e3
pape sembene <sembenpape4@gmail.com>
2025-07-23 10:06:05 +0000

    test deploiement

--- public/index.php
+++ public/index.php
@@ -1,14 +1,11 @@
-
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
 use App\Router\Router;
 
-
+header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
-
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
